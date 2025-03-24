dd982228c2187376124966b60fa1c27b7a1e3d03
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-03-24 12:59:22 +0530

    message

--- controlFlow/controlFlow.js
+++ controlFlow/controlFlow.js
@@ -62,3 +62,5 @@ let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 console.log("Authentication Status:", authenticationStatus);
+
+// Practice answer
