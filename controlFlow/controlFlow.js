e2c27430f925d8591af20b514b446e0ce5ecc203
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-03-24 13:13:56 +0530

    control flow practice ques

--- controlFlow/controlFlow.js
+++ controlFlow/controlFlow.js
@@ -64,3 +64,45 @@ let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticate
 console.log("Authentication Status:", authenticationStatus);
 
 // Practice answer
+/*
+Suppose an organization arranges a "Dietary Services" program to provide diets to its employees and customers, 
+based on a person's weight and day-to-day routine. You need to create an authorization-based code to provide 
+access to people based on their roles in the organization, such as employees, enrolled members for 
+"Dietary Services," and subscribers.
+
+If the person is an Employee, they are authorized to have access to "Dietary Services".
+
+If the person is an Enrolled Member, they are authorized to have access to "Dietary Services" and one-on-one 
+interaction with a dietician.
+
+If the person is a Subscriber, they are authorized to have partial access to facilitate "Dietary Services" only.
+
+If the person is a Non-Subscriber, they need to enroll or at least subscribe first to avail this facility.
+
+You need to communicate with the user by printing a message indicating whether that person is eligible to avail 
+which type of services.
+*/
+
+let program;
+let membertype = "Employee";
+
+switch(membertype)
+{
+    case "Employee":
+        program = "You have access to Dietary Services";
+        break;
+    case "Enrolled Member":
+        program = "You have access to and one-on-one interaction with a dietician";
+        break;
+    case "Subscriber":
+        program = "Only have access to facilitate Dietary Services";
+        break;
+    case "Non-Subscriber":
+        program = "Please enroll or at least subscribe first to avail this facility.";
+        break;
+    default:
+        program = "Invalid Role"
+        break;
+}
+
+console.log("Hello",membertype,program);
