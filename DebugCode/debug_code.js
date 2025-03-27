8eb134fcdbdca7532a65fbb2b8e1944da1b58825
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-03-27 18:11:00 +0530

    debug code division function added

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,18 +1,28 @@
     
-    function performOperation() {
+    function performOperation(operation) {
         // Get user input from input fields
         let num1 = parseInt(document.getElementById('input1').value);
         let num2 = parseInt(document.getElementById('input2').value);
         // Check if inputs are valid numbers
         if (!isNaN(num1) && !isNaN(num2)) {
         // Perform the operation
-                        let result = multiply(num1, num2);
-    
-                        // Display the result
-                        displayResult(result);
-                    } else {
-                        displayResult('Please enter valid numbers');
-                    }
+                        if (operation === 'multiply') {
+                            let result = multiply(num1, num2);
+                            // Display the result
+                            displayResult(result);
+                        } 
+                        else if(operation === 'division') {
+                            let result = divide(num1, num2);
+                            // Display the result
+                            displayResult(result);
+                        }
+                        else {
+                            displayResult('Please choose valid operation');
+                        }
+                }
+                else {
+                    displayResult('Please enter valid numbers');
+                }
                 }
     
                 function multiply(a, b) {
@@ -21,6 +31,14 @@
     
                     // Multiply the numbers
                     return a * b;
+                }
+
+                function divide(a, b) {
+                    // Introduce a debugger statement to pause execution
+                    debugger;
+    
+                    // Divide the numbers
+                    return a / b;
                 }
     
                 function displayResult(result) {
