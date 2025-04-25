3ec4f1ce60138d24b43248a8ddcf6d66082d7b9e
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-04-25 22:57:57 +0530

    Updated

---
+++ React/2_Function_Components_and_array_man/event_planner/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+
+        <footer>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque nulla rem incidunt voluptatem aut deserunt reprehenderit veritatis architecto soluta explicabo impedit, veniam obcaecati quidem dolorum. Dolorem deserunt pariatur temporibus laudantium.</p>
+        </footer>
+    )
+} 
+
+export default Footer
