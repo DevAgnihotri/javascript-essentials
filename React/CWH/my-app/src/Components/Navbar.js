98920a6461b132bf933756629c187e0d2e2dde33
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-06-02 16:43:51 +0530

    props

--- React/CWH/my-app/src/Components/Navbar.js
+++ React/CWH/my-app/src/Components/Navbar.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 
 
 
-export default function Navbar({ title = 'Set title here' }) {
+export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">{title}</a>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
