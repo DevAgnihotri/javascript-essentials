0d5e96906f664aa87736b30cc8964983b76025a2
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-06-06 20:05:34 +0530

    Removed prev and next Added Infinite Scroll

--- React/CWH/newsapp/src/components/Navbar.js
+++ React/CWH/newsapp/src/components/Navbar.js
@@ -2,28 +2,45 @@ import React, { Component } from 'react'
 import {
   Link
 }  from "react-router-dom";
-export class Navbar extends Component {
 
+export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavCollapsed: true
+    };
+  }
+
+  handleNavCollapse = () => {
+    this.setState({isNavCollapsed: !this.state.isNavCollapsed});
+  }
 
   render() {
+    const { isNavCollapsed } = this.state;
     return (
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">NewsMonkey</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <button 
+                  className="navbar-toggler" 
+                  type="button" 
+                  onClick={this.handleNavCollapse}
+                  aria-controls="navbarSupportedContent" 
+                  aria-expanded={!isNavCollapsed} 
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className={`collapse navbar-collapse ${!isNavCollapsed ? 'show' : ''}`} id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Home</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/">About</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>      
-                    {/* <a className="nav-link disabled" aria-disabled="true">Disabled</a> */}
+                    <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/" onClick={this.handleNavCollapse}>Home</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/" onClick={this.handleNavCollapse}>About</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/entertainment" onClick={this.handleNavCollapse}>Entertainment</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/business" onClick={this.handleNavCollapse}>Business</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/health" onClick={this.handleNavCollapse}>Health</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/science" onClick={this.handleNavCollapse}>Science</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/sports" onClick={this.handleNavCollapse}>Sports</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/technology" onClick={this.handleNavCollapse}>Technology</Link></li>      
                 </ul>
                 
                 </div>
--- React/CWH/newsapp/src/components/News.js
+++ React/CWH/newsapp/src/components/News.js
@@ -1,110 +1,79 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-import Spinner from './Spinner';
 import PropTypes from 'prop-types'
-
-
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Spinner from './Spinner'
 export class News extends Component {
-
-//     articles = {
-//   "status": "ok",
-//   "totalResults": 2,
-//   "articles": [
-//     {
-//       "source": {
-//         "id": "espn-cric-info",
-//         "name": "ESPN Cric Info"
-//       },
-//       "author": null,
-//       "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-//       "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-//       "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-//       "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-//       "publishedAt": "2020-04-27T11:41:47Z",
-//       "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-//     },
-//     {
-//       "source": {
-//         "id": "espn-cric-info",
-//         "name": "ESPN Cric Info"
-//       },
-//       "author": null,
-//       "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-//       "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-//       "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-//       "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-//       "publishedAt": "2020-03-30T15:26:05Z",
-//       "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-//     }
-//   ]
-// };
-    
-    
-    static defaultProps = {
+   
+      static defaultProps = {
     country: 'us',
     pageSize: 8,
     category: 'entertainment'
-    
-  }
-    static propTypes = {
+  };
+  
+  static propTypes = {
       country: PropTypes.string,
       pageSize: PropTypes.number,
       category: PropTypes.string,
-    }
-
-    constructor () {
-        super();
+    };
+    
+    capitalizeFirstLetter = (string) => {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+      constructor (props) {
+        super(props);
         this.state = {
             articles: [],
             loading: false,
-            page: 1
+            page: 1,
+            totalResults: 0,
+            hasMoreData: true
         }
+
+        document.title= `NewsMonkey - ${this.capitalizeFirstLetter(this.props.category)}`
     }
 
-  async updateNews() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f9be7e046f7b47f0aa0b639157b646a0&page=${this.state.page}&pageSize=${this.props.pageSize}`
+  async componentDidMount() {
+    this.fetchMoreData()
+  }
+  fetchMoreData = async () => {
+    this.setState({page: this.state.page + 1})
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f9be7e046f7b47f0aa0b639157b646a0&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
     this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({articles: parsedData.articles,
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
        totalResults: parsedData.totalResults,
-      loading: false})
-  }
-
-  async componentDidMount() {
-    this.updateNews()
-  }
+      loading: false,
+      hasMoreData: parsedData.articles.length > 0
+    })
+  };
   
-  handlePrevClick = async () => {
-      
-      this.setState({page: this.state.page - 1})
-      this.updateNews()
-  }
-
-  handleNextClick = async () => {
-    this.setState({page: this.state.page + 1})
-    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))) 
-    {
-    this.updateNews()
-    }
-  }
   render() {
     return (
-      <div className='container my-3'>
-        <h1  className='text-center' style={{margin: "40px 0px"}}>Top Headlines</h1>
-       {this.state.loading && <Spinner/>}
-        <div className="row">
-            {!this.state.loading && this.state.articles.map((element) => {
+      <>
+        <h1  className='text-center' style={{margin: "40px 0px"}}>Top Headlines from {this.capitalizeFirstLetter(this.props.category)} category</h1>        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMoreData}
+          loader={ <Spinner/> }
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          <div className='container my-3'>
+          <div className="row">
+            {this.state.articles.map((element) => {
                 return <div className="col-md-4" key={element.url}>
                 <NewsItem title={element.title?element.title.slice(0,25):""} description={element.description?element.description.slice(0,88):""} time={element.publishedAt?element.publishedAt:"Not mentioned"} author={element.author?element.author:""} imageUrl={element.urlToImage?element.urlToImage:"https://plus.unsplash.com/premium_photo-1707080369554-359143c6aa0b?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bmV3cyUyMHdlYnNpdGV8ZW58MHx8MHx8fDA%3D"} newsUrl={element.url}/>
             </div>
             })}
-        </div>
-        <div className="container d-flex justify-content-around">
-          <button disabled={this.state.page<1} type='button' className='btn btn-dark' onClick={this.handlePrevClick}> &larr; Prev</button>
-          <button disabled={this.state.page >= Math.ceil(this.state.totalResults/this.props.pageSize)} type='button' className='btn btn-warning' onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
-      </div>
+          </div></div>
+        </InfiniteScroll>
+      </>
     )
   }
 }
